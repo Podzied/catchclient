c0b89e91d23c1730a34f98f3df9b64c942a92f08
Charge <77408912+Podzied@users.noreply.github.com>
2022-12-26 04:03:59 -0600

    Build Setup File

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='catchclient',
+    version='1.0.0',
+    url='https://github.com/podzied/catchclient',
+    author='Podzied',
+    description='Email Catch-All Client and API Wrapper for emailfake.com',
+    packages=find_packages(),    
+    install_requires=['requests >= 2.28.1', 'bs4 >= 0.01', 'retrying >= 1.3.3'],
+)
